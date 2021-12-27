ebf9091337cf46ef46f0e19bc93d7110499417c3
Davi1234567891099 <dsavianomoraes@gmail.com>
2021-12-26 21:39:41 -0300

    inserindo usuário

--- src/main/java/com/davi/workshopmongo/service/UserService.java
+++ src/main/java/com/davi/workshopmongo/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davi.workshopmongo.domain.User;
+import com.davi.workshopmongo.dto.UserDTO;
 import com.davi.workshopmongo.repository.UserRepository;
 import com.davi.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
 		catch(RuntimeException e) {
 			throw new ObjectNotFoundException("Object not exists.");
 		}
+	}
+	
+	public User insert(User obj) {
+		return ur.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
