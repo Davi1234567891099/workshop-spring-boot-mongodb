eebf152ab909b812b9095e1577f13acd7c59c1ed
Davi1234567891099 <dsavianomoraes@gmail.com>
2021-12-26 21:57:52 -0300

    deletando usuario

--- src/main/java/com/davi/workshopmongo/service/UserService.java
+++ src/main/java/com/davi/workshopmongo/service/UserService.java
@@ -36,4 +36,9 @@ public class UserService {
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		ur.deleteById(id);
+	}
 }
